488dc4d10b38e2953c17db6741153e85a72519cf
MCFigue <daniel.j.f.m@hotmail.com>
2022-02-06 18:30:23 -0500

    Cambio en delete

--- delete_task.php
+++ delete_task.php
@@ -10,7 +10,7 @@ if(isset($_GET['id'])) {
     die("Query Failed.");
   }
 
-  $_SESSION['message'] = 'Tarea removida exitosamente';
+  $_SESSION['message'] = 'Tarea eliminada exitosamente';
   $_SESSION['message_type'] = 'danger';
   header('Location: index.php');
 }
